073ea25e0d4b1c2e1ae31ebd124f5606f166e1c1
Jan Heiland <jnhlnd@gmail.com>
2020-10-13 10:47:43 +0200

    now the scripts seem like complete

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -13,11 +13,17 @@ python3 pplot_dc_lcurve_optinf.py
 python3 pplot_dc_timedomain_error.py
 # generates Fig. 7.6
 
+python3 pplot_dc_pressure_approximation.py
+# generates Fig. 7.6
+
 python3 pplot_dc_error_vs_rom.py
 # generates Fig. 7.8
 
 python3 pplot_cw_pod_sv_decay.py
 # generates Fig. 7.9
+
+python3 pplot_cw_timedomain_error.py
+# generates Fig. 7.10.
 
 python3 pplot_cw_pressure_approx_veldiffs.py
 # generates Fig. 7.11 and the data for 7.13
