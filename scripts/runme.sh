9e109e2f7728486d1f917ca21f792ae532887683
Jan Heiland <jnhlnd@gmail.com>
2020-10-13 09:20:46 +0200

    now using the reference-data

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,5 +1,9 @@
 export PYTHONPATH="../"
 
+# ## uncomment to use the reference dat
+# mkdir results
+# cp reference-data/*.json results/
+
 python3 pplot_dc_pod_sv_decay.py
 # generates Fig. 7.4
 
@@ -11,3 +15,8 @@ python3 pplot_dc_timedomain_error.py
 
 python3 pplot_dc_error_vs_rom.py
 # generates Fig. 7.8
+
+python3 pplot_cw_pod_sv_decay.py
+# generates Fig. 7.9
+
+python3 pplot_cw_error_vs_rom.py
